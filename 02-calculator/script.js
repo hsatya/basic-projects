39b48a7c6a12d9fbff33eabfaa5813212f773e21
Harshit Satya <harshitsatya5@gmail.com>
2024-03-30 20:10:19 +0530

    update and structure calculator project 2

--- 02-calculator/script.js
+++ 02-calculator/script.js
@@ -2,5 +2,81 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clear = document.getElementById("clear-btn");
 
+// Calculate first and second value depending upon operator
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
 
-function sendNumberValue() {}
+let firstValue = 0;
+let operatorValue = "";
+let awaitingNextValue = false;
+
+function sendNumberValue(number) {
+  // Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    // If current display value is 0, replace it, if not add number to display value
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === "0" ? number : displayValue + number;
+  }
+}
+
+function addDecimal() {
+  // If operator pressed don't add decimal
+  if (awaitingNextValue) return;
+  // If no decimal, add one
+  if (!calculatorDisplay.textContent.includes(".")) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
+  // Assign first value only if no value
+  if (!firstValue) {
+    console.log("first Value");
+    firstValue = currentValue;
+  } else {
+    console.log(firstValue, operatorValue, currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    console.log("Calculation", calculation);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
+  // Raedy for the next Value, store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+}
+
+// Reset all values display
+function resetAll() {
+  firstValue = 0;
+  operatorValue = "";
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = "0";
+}
+
+// Add event listners for numbers, operators, decimal button
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => useOperator(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", addDecimal);
+  }
+});
+
+clear.addEventListener("click", resetAll);
